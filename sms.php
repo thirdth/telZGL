94e358a5a6f20ca3f528b380ca69bb44b0ac6a43
Zack Glaser <zackglaser@gmail.com>
2018-01-25 12:24:42 -0600

    saved file

--- sms.php
+++ sms.php
@@ -3,7 +3,5 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Gather numDigits="1" action="transfer.php" method="POST">
-      <Say voice="alice" language="en">Welcome to Zack Glaser Legal. If you would like to speak to a representative, please press 1, now</Say>
-    </Gather>
+    <Message to="<?=$_REQUEST['PhoneNumber']?>"> <?=htmlspecialchars(substr($_REQUEST['From'] . ": " . $_REQUEST['Body'], 0, 1600))?> </Message>
 </Response>
